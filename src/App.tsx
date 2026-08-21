e311a2a1ab3e30ed6939d0cb1bbf1d494b8db3c4
Showxxx <ishowxpremium@gmail.com>
2026-08-21 23:53:41 +0200

    Bring elastic cursor rope on top of login card by increasing z-index

--- src/App.tsx
+++ src/App.tsx
@@ -3642,7 +3642,7 @@ export default function App() {
                   width: '100%',
                   height: '100%',
                   pointerEvents: 'none',
-                  zIndex: 5
+                  zIndex: 15
                 }}
               >
                 <defs>
